69a7d439589f8d85a37b90c250ea87d415ff1743
Denis-tech-jpg <oyugidenis82@gmail.com>
2025-05-12 15:43:40 +0300

    Week 8 Final Project

---
+++ css/js/script.js
@@ -0,0 +1,14 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+  
+    if (!name || !email || !message) {
+      alert("All fields are required.");
+      e.preventDefault();
+    } else if (!email.includes("@")) {
+      alert("Please enter a valid email address.");
+      e.preventDefault();
+    }
+  });
+  
